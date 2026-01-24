4417a0740e7e1d79411713cb62f459355bac3979
mindbodyclaritys-eng <mindbodyclaritys@gmail.com>
2026-01-24 13:01:15 +0500

    ok

---
+++ booking.js
@@ -0,0 +1,11 @@
+ // GET PLAN FROM LOCAL STORAGE
+        const plan = localStorage.getItem("selectedPlan");
+        const price = localStorage.getItem("planPrice");
+        const days = localStorage.getItem("planDays");
+
+        if (plan) {
+            document.getElementById("planName").innerText = plan;
+            document.getElementById("planPrice").innerText = "$" + price;
+            document.getElementById("totalPrice").innerText = "$" + price;
+            document.getElementById("planDays").innerText = days || "—";
+        }
