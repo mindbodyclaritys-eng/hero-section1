669e43c6f4e9b04a84378084d8cbafc3b382e92a
mindbodyclaritys-eng <mindbodyclaritys@gmail.com>
2026-01-26 11:17:50 +0500

    dd

--- booking.js
+++ booking.js
@@ -2,10 +2,13 @@
         const plan = localStorage.getItem("selectedPlan");
         const price = localStorage.getItem("planPrice");
         const days = localStorage.getItem("planDays");
-
+        
         if (plan) {
             document.getElementById("planName").innerText = plan;
             document.getElementById("planPrice").innerText = "$" + price;
             document.getElementById("totalPrice").innerText = "$" + price;
             document.getElementById("planDays").innerText = days || "—";
         }
+        const bookbtn = document.querySelector('.book-btn').addEventListener('click',()=>{
+            alert('form has submitted successfully')
+        })
